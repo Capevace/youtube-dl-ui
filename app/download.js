7088bbd5a5cf4f7e9328d47000e6d96c00e91b92
Lukas von Mateffy <luke@mateffy.me>
2020-05-27 19:53:58 +0200

    Added check for download directory

--- app/download.js
+++ app/download.js
@@ -3,20 +3,28 @@ const promisify = require('util').promisify;
 const sanitizeUrl = require('@braintree/sanitize-url').sanitizeUrl;
 
 const { spawn } = require('child_process');
+const fs = require('fs');
 
 function download(url, options = {}, onOutput = () => {}) {
 	return new Promise((resolve, reject) => {
 		console.log('Downloading', url);
 
 		const safeUrl = sanitizeUrl(url);
+		const cwd = options.extractAudio ? config.audioPath : config.videosPath;
+
+		if(!fs.existsSync(cwd)) {
+			return reject(new Error('The specified download directory: ' + cwd + ' does not exist.'));
+		}
 
 		const downloadProcess = spawn(
 			'youtube-dl', 
 			options.extractAudio 
 				? ['-x', '--audio-format', 'm4a', safeUrl]
 				: ['--format', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', safeUrl],
-			{ cwd: options.extractAudio ? config.audioPath : config.videosPath }
+			{ cwd }
 		);
+
+		onOutput('[YTDL] Starting download...');
 
 		let stdout = '';
 		downloadProcess.stdout.on('data', (line) => {
@@ -28,12 +36,12 @@ function download(url, options = {}, onOutput = () => {}) {
 
 		let stderr = '';
 		downloadProcess.stderr.on('data', (data) => {
-			console.log('error while downloading:', data);
 			stderr += data;
 		});
 
 		downloadProcess.on('close', (code) => {
 			if (stderr !== '') {
+				console.log(stderr);
 				reject(new Error(String(stderr)));
 			} else {
 				resolve();
