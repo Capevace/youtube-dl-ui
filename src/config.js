9f2ee69a6ccbedea9735e6b1c3d92cac2502054c
Lukas Mateffy <github@mateffy.me>
2021-12-20 19:27:18 +0100

    Updated configuration log

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+const package = require('../package.json');
 const fs = require('fs');
 const argv = require('./argv');
 
@@ -14,14 +15,10 @@ const config = require('rc')('youtube-dl-ui', {
 	debug: false,
 
 	http: {
-		// url: 'http://localhost:3003',
 		socketPath: '/socket.io',
 		port: 3003,
 	},
-	// missionControl: {
-	// 	url: 'http://localhost:3000',
-	// 	apiKey: ''
-	// },
+
 	videoPath: defaultVideoPath,
 	audioPath: defaultAudioPath,
 	youtubeDl: 'youtube-dl',
@@ -36,32 +33,6 @@ config.http.socketPath =
 config.videoPath = argv.videoPath || process.env.VIDEO_PATH || config.videoPath;
 config.audioPath = argv.audioPath || process.env.AUDIO_PATH || config.audioPath;
 config.youtubeDl = argv.youtubeDl || process.env.YOUTUBE_DL || config.youtubeDl;
-
-// Create base config file
-// (taken out for now, I don't think it's needed but let's see) - 2021-11-04
-
-// if (!fs.existsSync(config.basePath)) {
-// 	console.log('Base path not found. Creating base directory ' + config.basePath);
-
-// 	fs.mkdirSync(config.basePath, { recursive: true });
-// }
-
-// if (!fs.existsSync(config.basePath + '/config')) {
-// 	console.log('Config file not found. Creating...');
-
-// 	fs.writeFileSync(config.basePath + '/config',
-// `; YTDL Config File
-// ; Enable debug mode here
-// ;debug=true
-
-// ;videoPath=
-// ;audioPath=
-
-// ;[http]
-// ;port=3003
-// `
-// 	);
-// }
 
 // Create download directories, if no custom ones are passed and the default ones don't exist
 if (config.videoPath === defaultVideoPath && !fs.existsSync(defaultVideoPath)) {
@@ -80,11 +51,12 @@ if (config.audioPath === defaultAudioPath && !fs.existsSync(defaultAudioPath)) {
 
 // Log configuration
 console.log('=== Using Configuration ===');
-console.log(`Debug Mode: ${config.debug ? 'Enabled' : 'Disabled'}`);
+console.log(`Debug Mode:      ${config.debug ? 'Enabled' : 'Disabled'}`);
+console.log(`Version:         v${package.version}`);
+console.log(`HTTP port: 	`, config.http.port); // sperate argument, for number color
+console.log(`YouTube-DL path: ${config.youtubeDl}`);
 console.log(`Video directory: ${config.videoPath}`);
 console.log(`Audio directory: ${config.audioPath}`);
-console.log(`HTTP port: 	`, config.http.port); // sperate argument, for number color
-console.log(`YouTube-DL path: ${config.http.socketPath}`);
 console.log(`Socket URL path: ${config.http.socketPath}`);
 console.log('');
 
